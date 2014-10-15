a461f06c0264b7dbf4b648230c6853efa88c5626
Harrison <hs750@york.ac.uk>
2014-10-15 11:15:11 +0100

    congo website post practical 1

--- web2py-2/parameters_8000.py
+++ web2py-2/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a5badfb8b4a172c9$2005e0e4ea08cae6d325bebfe51076e4af4834af"
+password="pbkdf2(1000,20,sha512)$b477a8b802ccc93c$75e1f738a49423e733c47a9d80b57b2576fcff4e"

abb3fdf681ca875eb5cbc4d5514293d496b75323
Harrison <hs750@york.ac.uk>
2014-10-24 14:54:06 +0100

    post practical 3

--- web2py/applications/congo/controllers/default.py
+++ web2py/applications/congo/controllers/default.py
@@ -1,2 +1,70 @@
 def index():
     return dict()
+
+def search():
+    return dict()
+
+def search2():
+    form = FORM('Search Products',
+                INPUT(_type='search', _name='search', requires=IS_NOT_EMPTY()),
+                INPUT(_type='submit'))
+    results = None
+    if form.accepts(request.post_vars, session) :
+        response.flash = "Results displayed"
+        results = db(db.products.name.contains(request.post_vars.search)).select()
+    elif form.errors:
+        response.flash = "There was an error: your form was empty"
+    else:
+        response.flash = "please enter a search"
+    return dict(form=form, results=results)
+
+def addProduct():
+    form=FORM('Add Product',
+              DIV(LABEL('Name:', _for='Name')),
+              DIV(INPUT(_name='Name', requires=IS_NOT_EMPTY())),
+              DIV(LABEL('Price', _for='Price')),
+              DIV(INPUT(_name='Price', _type='number', requires=IS_NOT_EMPTY(), min=0, step=0.01)),
+              DIV(LABEL('Media Type:', _for='Type')),
+              DIV(SELECT('Book', 'Blu-Ray', _name='Type')),
+              DIV(LABEL('Description', _for='Description')),
+              DIV(TEXTAREA(_name='Description', requires=IS_NOT_EMPTY())),
+              DIV(LABEL('Publisher:', _for='Publisher')),
+              DIV(INPUT(_name='Publisher', requires=IS_NOT_EMPTY())),
+              DIV(INPUT(_name='submit', _type='submit')))
+
+    if form.accepts(request.post_vars, session):
+        response.flash = "Added to database..."
+        db.products.insert(name=request.post_vars.Name,
+                           price=request.post_vars.Price,
+                           ptype=request.post_vars.Type,
+                           description=request.post_vars.Description,
+                           publisher=request.post_vars.Publisher)
+
+    elif form.errors:
+        response.flash += "Make sure all fields are filled"
+    else:
+        response.flash = "Please fill out the form..."
+
+    return dict(form=form);
+
+def addProduct2():
+    db.products.description.widget = SQLFORM.widgets.text.widget
+    if request.args(0) is not None:
+        form = SQLFORM(db.products, db.products(request.args(0)))
+    else:
+        form=SQLFORM(db.products)
+
+    if form.accepts(request.post_vars, session):
+        response.flash = "Added to database..."
+        db.products.insert(name=request.post_vars.name,
+                           price=request.post_vars.price,
+                           ptype=request.post_vars.ptype,
+                           description=request.post_vars.description,
+                           publisher=request.post_vars.publisher)
+
+    elif form.errors:
+        response.flash += "Make sure all fields are filled"
+    else:
+        response.flash = "Please fill out the form..."
+
+    return dict(form=form);
--- web2py/applications/congo/controllers/store.py
+++ web2py/applications/congo/controllers/store.py
@@ -7,13 +7,14 @@ def index():
 
 
 def books():
+    if request.args(0) is not None:
+        try:
+            int(request.args(0))
+            return dict(books=db(db.products.id == request.args(0)).select())
+        except ValueError:
+            pass
     return dict(books=db(db.products.ptype == 'Book').select())
 
 
 def videos():
     return dict()
-
-features = dict( book1={'id': '1', 'name': 'Batman: Arkham Asylum',
-'price': '9.00', 'type': 'Book', 'writer': 'Grant Morrison','pages': '216', 'description': 'Some stuff about batman ...' },
-                book2={'id': '2', 'name': 'Superman',
-'price': '7.00', 'type': 'paperback book', 'writer': 'Some Guy','pages': '159', 'description': 'Some stuff about SUPERMAN ...'})
